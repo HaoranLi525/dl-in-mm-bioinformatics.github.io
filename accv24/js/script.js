0851b29f43b0b3b44ad3160df6d0a88c2dfcb4cc
HaoranLi525 <141290138+HaoranLi525@users.noreply.github.com>
2024-07-31 20:37:23 +1000

    Delete accv24 directory

--- accv24/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSubMenu() {
-    var subMenu = document.getElementById('infoSubMenu');
-    if (subMenu.style.display === 'block') {
-        subMenu.style.display = 'none';
-    } else {
-        subMenu.style.display = 'block';
-    }
-}
